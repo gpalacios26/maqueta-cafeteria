e47925f818fd8bd2773ee27d8328cd2ea60fc0b6
Gregory Palacios <gregpalacios26@gmail.com>
2022-01-15 11:03:59 -0500

    Primer commit web responsive café

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const { src, dest, watch, series } = require('gulp');
+
+// CSS y SASS
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function css() {
+    return src('src/scss/app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'));
+}
+
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'));
+}
+
+function versionWebp() {
+    const opciones = {
+        quality: 50
+    };
+
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+}
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    };
+
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
+}
+
+function dev() {
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
+}
+
+exports.css = css;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = dev;
+exports.default = series(css, imagenes, versionWebp, versionAvif, dev);
